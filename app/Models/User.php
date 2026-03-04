c95b41434d2009dc6dd3ba9464236bbcdce812e8
rob1423 <lacrearniel92@gmail.com>
2026-03-04 17:04:24 +0800

    Add Sanctum authentication and Spatie authorization

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Laravel\Sanctum\HasApiTokens; // ADD THIS
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasApiTokens, HasFactory, Notifiable; // ADD HasApiTokens HERE
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
--- routes/api.php
+++ routes/api.php
@@ -1,15 +1,4 @@
 <?php
-use App\Http\Controllers\AuthController;
-
-Route::post('/register', [AuthController::class,'register']);
-Route::post('/login', [AuthController::class,'login']);
-
-Route::middleware('auth:sanctum')->group(function () {
-
-    Route::post('/logout', [AuthController::class,'logout']);
-
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
-
-});
+Route::get('/admin-dashboard', function () {
+    return "Welcome Admin";
+})->middleware(['auth:sanctum', 'role:admin']);
